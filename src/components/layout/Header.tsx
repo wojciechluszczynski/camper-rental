61602e989c5e3cd4a7c12ac90cbbe9fa4d527fe1
Wojciech <wojciech@MacBook-Pro-Wojciech-5.local>
2026-04-04 19:41:16 +0200

    feat: redesign header with SVG logo icon, sticky blur

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,21 @@
 import Link from 'next/link'
+import { IconCamper } from '@/components/icons'
 import styles from './Header.module.css'
 
 export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
-        <Link href="/" className={styles.logo}>Kamperownia</Link>
+        <Link href="/" className={styles.logo}>
+          <IconCamper size={22} />
+          <span>Kamperownia</span>
+        </Link>
         <nav className={styles.nav}>
-          <Link href="#fleet">Kamery</Link>
-          <Link href="#how-it-works">Jak to działa</Link>
-          <Link href="#contact">Kontakt</Link>
+          <Link href="/#fleet">Kamery</Link>
+          <Link href="/#jak-to-dziala">Jak to działa</Link>
+          <Link href="/#kontakt">Kontakt</Link>
         </nav>
-        <Link href="#fleet" className={styles.cta}>Zarezerwuj</Link>
+        <Link href="/#fleet" className={styles.cta}>Zarezerwuj</Link>
       </div>
     </header>
   )
